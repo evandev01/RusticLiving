ba704962fb0001ec29b28d8a0c8af3151c58aa20
Bears <evanbero1980@gmail.com>
2021-06-23 22:20:28 -0500

    added remove from cart functionality

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,11 +1,14 @@
-import { CART_ADD_ITEM } from '../constants/cartConstants'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
 // import { CART_REMOVE_ITEM } from '../constants/cartConstants'
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
     case CART_ADD_ITEM:
       const item = action.payload
-
+      ////////////////////////// TAKE NOTES FIRST THING //////////////////////
+      ////////////////////////// TAKE NOTES FIRST THING //////////////////////
+      ////////////////////////// TAKE NOTES FIRST THING //////////////////////
+      ////////////////////////// TAKE NOTES FIRST THING //////////////////////
       const existItem = state.cartItems.find(x => x.product === item.product)
 
       if (existItem) {
@@ -21,7 +24,12 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
           cartItems: [...state.cartItems, item],
         }
       }
-
+    case CART_REMOVE_ITEM:
+      return {
+        ...state,
+        // Filter out the id of whatever we remove
+        cartItems: state.cartItems.filter(x => x.product !== action.payload),
+      }
     default:
       return state
   }
