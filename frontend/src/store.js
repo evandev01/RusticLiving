ac9346b2a0722be20531b16e4f4c731c68992604
Bears <evanbero1980@gmail.com>
2021-06-25 15:38:18 -0500

    added user login constants, reducers, actions

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,20 +6,28 @@ import {
   productListReducer,
 } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducer } from './reducers/userReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 })
 
-// Gets items from local storage and parses them back from string(in cartAction) to JavaScript format
+// Gets items from local storage and parses them back from string(in cartActions) to JavaScript format
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : []
 
+// Gets user info from local storage and parses them back from string(in userActions) to JavaScript format
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null
+
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 }
 
 const middleware = [thunk]
