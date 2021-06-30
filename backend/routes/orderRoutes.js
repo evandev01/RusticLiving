f8337c0a4363f3369cf4954158cbcfd5c1188922
Bears <evanbero1980@gmail.com>
2021-06-30 10:54:41 -0500

    added controller, route, action, reducer, frontend on profile for getting user orders

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,12 +4,14 @@ import {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
+  getMyOrders,
 } from '../controllers/orderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 // Calls functions in controller
 
 router.route('/').post(protect, addOrderItems)
+router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
 
