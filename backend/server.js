832fb91c34efd7c8f57f8e33a091703979031c8f
Bears <evanbero1980@gmail.com>
2022-02-10 09:45:28 -0600

    adding maps

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-// import sgMail from '@sendgrid/mail'
 import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
@@ -27,27 +26,6 @@ app.use(express.json())
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
-
-// using Twilio SendGrid's v3 Node.js Library
-// https://github.com/sendgrid/sendgrid-nodejs
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-// const msg = {
-//   to: 'evanbero@evandev.com', // Change to your recipient
-//   from: 'evanbero@evandev.com', // Change to your verified sender
-//   subject: 'Sending with SendGrid is Fun',
-//   text: 'and easy to do anywhere, even with Node.js',
-//   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-// }
-// sgMail.send(msg).then(
-//   () => {},
-//   error => {
-//     console.error(error)
-
-//     if (error.response) {
-//       console.error(error.response.body)
-//     }
-//   }
-// )
 
 app.use('/api/products', productRoutes)
 app.use('/api/gallery', galleryRoutes)
