be287e00f45c3d87cf80a71ec1bf299bbb84270f
Bears <evanbero1980@gmail.com>
2022-02-10 13:49:25 -0600

    added facebook

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,16 +1,27 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Image } from 'react-bootstrap'
+import FB_LOGO from '../assets/logos/fb_logo.png'
 
 const Footer = () => {
   return (
     <footer>
       <Container className='text-center mt-5'>
         <Row>
-          <Col className='text-center'>
-            <p className='text-center mt-3'>
+          <Col className='text-center p-3'>
+            <p
+              className='text-center'
+              style={{
+                textDecoration: 'none',
+                fontSize: '1.2em',
+              }}
+            >
               <a
                 href='https://www.google.com/search?q=rustic+living+hobart+indiana'
-                style={{ textDecoration: 'none' }}
+                target='_blank'
+                style={{
+                  textDecoration: 'none',
+                  fontSize: '1em',
+                }}
               >
                 Rustic Living
                 <br />
@@ -19,18 +30,22 @@ const Footer = () => {
                 Hobart, IN 46342
               </a>
               <br />
-              <a href='tel:+12197131371' style={{ textDecoration: 'none' }}>
+              <a
+                href='tel:+12197131371'
+                style={{ textDecoration: 'none', fontSize: '1em' }}
+              >
                 (219)713-1371
               </a>
             </p>
           </Col>
         </Row>
+
         <Row>
-          <Col className='text-center p-3'>
+          <Col className='text-center p-2'>
             <iframe
               src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11946.727473261486!2d-87.25369!3d41.5328313!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3a308cdfcdfbeed4!2sRustic%20Living!5e0!3m2!1sen!2sus!4v1644508055272!5m2!1sen!2sus'
-              width='400'
-              height='auto'
+              height='250px'
+              width='auto'
               style={{ border: '0' }}
               allowfullscreen='true'
               loading='lazy'
@@ -38,8 +53,26 @@ const Footer = () => {
             ></iframe>
           </Col>
         </Row>
+
         <Row>
-          <Col className='text-center pb-3'>Copyright &copy; Rustic Living</Col>
+          <Col className='text-center p-3'>
+            <p className='text-center'>
+              Join us on Facebook!
+              <br />
+              <a
+                href='https://www.facebook.com/RusticLivingNWI/'
+                target='_blank'
+              >
+                <Image
+                  src={FB_LOGO}
+                  style={{ maxHeight: '100px', width: 'auto' }}
+                />
+              </a>
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col className='text-center'>Copyright &copy; Rustic Living</Col>
         </Row>
       </Container>
     </footer>
