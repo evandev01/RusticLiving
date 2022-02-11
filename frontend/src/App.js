c9bc4b470c192dec859619f07e5dd7f0d7989793
Bears <evanbero1980@gmail.com>
2022-02-11 16:28:53 -0600

    styled footer, mobile responsive

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
+import './index.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
     <footer>
       <Container className='text-center mt-5'>
         <Row>
-          <Col className='text-center p-3'>
+          <Col className='text-center'>
             <p
               className='text-center'
               style={{
@@ -20,6 +20,7 @@ const Footer = () => {
               <a
                 href='https://www.google.com/search?q=rustic+living+hobart+indiana'
                 target='_blank'
+                rel='noreferrer'
                 style={{
                   textDecoration: 'none',
                   fontSize: '1em',
@@ -42,7 +43,7 @@ const Footer = () => {
           </Col>
         </Row>
 
-        <Row>
+        <Row style={{ borderBottom: 'solid black 1px', padding: '20px' }}>
           <Col className='text-center p-2'>
             <iframe
               src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11946.727473261486!2d-87.25369!3d41.5328313!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3a308cdfcdfbeed4!2sRustic%20Living!5e0!3m2!1sen!2sus!4v1644508055272!5m2!1sen!2sus'
@@ -56,50 +57,41 @@ const Footer = () => {
           </Col>
         </Row>
 
-        <Row>
+        <Row style={{ padding: '20px' }}>
           <Col className='text-center p-3'>
-            <p className='text-center'>
+            <p className='text-center' style={{ fontSize: '1em' }}>
               <a
                 href='https://www.facebook.com/RusticLivingNWI/'
                 target='_blank'
+                rel='noreferrer'
               >
-                <Image
-                  alt='Facebook'
-                  src={FB_LOGO}
-                  style={{ maxHeight: '100px', width: 'auto' }}
-                />
+                <Image id='icon' alt='Facebook' src={FB_LOGO} />
               </a>
               <br />
               Facebook
             </p>
           </Col>
           <Col className='text-center p-3'>
-            <p className='text-center'>
+            <p className='text-center' style={{ fontSize: '1em' }}>
               <a
                 href='https://www.instagram.com/rustic_living_life/'
                 target='_blank'
+                rel='noreferrer'
               >
-                <Image
-                  alt='Instagram'
-                  src={IG_LOGO}
-                  style={{ maxHeight: '100px', width: 'auto' }}
-                />
+                <Image id='icon' alt='Instagram' src={IG_LOGO} />
               </a>
               <br />
               Instagram
             </p>
           </Col>
           <Col className='text-center p-3'>
-            <p className='text-center'>
+            <p className='text-center' style={{ fontSize: '1em' }}>
               <a
                 href='https://www.tiktok.com/search?lang=en&q=rusticlivingnwi&t=1644523117731'
                 target='_blank'
+                rel='noreferrer'
               >
-                <Image
-                  alt='TikTok'
-                  src={TT_LOGO}
-                  style={{ maxHeight: '100px', width: 'auto' }}
-                />
+                <Image id='icon' alt='TikTok' src={TT_LOGO} />
               </a>
               <br />
               TikTok
