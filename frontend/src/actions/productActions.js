792299ccca06cdecc6ef170f56eb638f9d8db7b0
Evan Bero <63209633+evandev01@users.noreply.github.com>
2022-03-08 13:33:19 -0600

    fixed create product bug

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -99,7 +99,7 @@ export const deleteProduct = id => async (dispatch, getState) => {
   }
 }
 
-export const createProduct = () => async (dispatch, getState) => {
+export const createProduct = product => async (dispatch, getState) => {
   try {
     dispatch({
       type: PRODUCT_CREATE_REQUEST,
@@ -116,7 +116,7 @@ export const createProduct = () => async (dispatch, getState) => {
       },
     }
 
-    const { data } = await axios.post(`/api/products`, {}, config)
+    const { data } = await axios.post(`/api/products`, product, config)
 
     dispatch({
       type: PRODUCT_CREATE_SUCCESS,
